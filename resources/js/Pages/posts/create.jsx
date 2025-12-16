7ab3ce8e61cc470c292a2789ab1dabd9ce329bcd
Rohit Lad <rohit.lad.sae@gmail.com>
2025-12-16 22:30:11 +0100

    clsx added

--- resources/js/Pages/posts/create.jsx
+++ resources/js/Pages/posts/create.jsx
@@ -1,5 +1,6 @@
 import AppLayout from "@/layouts/app-layout";
 import { Form } from "@inertiajs/react";
+import {cn} from "@/lib/utils";
 
 export default function PostsShow() {
     return (
@@ -11,11 +12,23 @@ export default function PostsShow() {
                         <>
                             <div>
                                 <label htmlFor="title" className="block mb-1">Title</label>
-                                <input type="text" id="title" name="title" required className="w-full border rounded px-3 py-2" />
+                                <input type="text" id="title" name="title" required className={
+                                    cn(
+                                        "w-full border rounded px-3 py-2",
+                                        errors.title && "border-red-500"
+                                    )
+                                } />
+                                {errors.title && <p className="text-red-500 text-sm mt-1">{errors.title}</p>}
                             </div>
                             <div>
                                 <label htmlFor="body" className="block mb-1">Body</label>
-                                <textarea id="body" name="body" required className="w-full border rounded px-3 py-2" />
+                                <textarea id="body" name="body" required className={
+                                    cn(
+                                        "w-full border rounded px-3 py-2",
+                                        errors.body && "border-red-500"
+                                    )
+                                } />
+                                {errors.body && <p className="text-red-500 text-sm mt-1">{errors.body}</p>}
                             </div>
                             <button type="submit" className="bg-blue-600 text-white px-4 py-2 rounded">
                                 Create
