249fe9df408401187f9844e38159a65ba114b4bf
Rohit Lad <rohit.lad.sae@gmail.com>
2025-12-16 10:52:41 +0100

    basic pages added

--- resources/js/Pages/about.jsx
+++ resources/js/Pages/about.jsx
@@ -2,7 +2,7 @@ import { Link } from "@inertiajs/react";
 
 export default function Home({}){
     return <div>
-        <h1>Home</h1>
-        <Link href='/about'>About</Link>
+        <h1>About</h1>
+        <Link href='/'>Home</Link>
     </div>;
 }
